e7a982cd189e91e0eb5d3a4562ee8ee791ac7797
Christian Figueroa <figgchristian@gmail.com>
2017-03-15 00:17:29 -0700

    Better context menu keyboard control, new unwrap element option, and bug fixes
    Fixed a typo from pesudoContextMenuItem to pseudoContextMenuItem
    
    The sub menus (File, Edit, etc.) now have better key board control:
    - The user can now tab to select a submenu button and press enter to
    open/close it.
    - The user can also use down arrow key to open the context menun and
    select the first item
    - All four arrow keys are used in context menus now:
    - Down selects the next item down, or the first item if there are no
    more items below the current one
    - Up selects the next item up, or the last item if there are no more
    items above the current one. With submenus, the text to open the submenu
    is selected instead (Edit is selected instead Preferences inside the
    Edit submenu)
    - Left will select the subcontextmenu of the item if one is defined for
    the item
    - Right will select the subcontextmenu's parent item if one is defined
    - The arrow kes also work for the text buttons that open submenus
    
    - New Ctrl + H shortcut to edit the HTML of an element
    
    - New Unwrap Element option in the right click context menu. The
    selected node is replaced with all its children (useful for replacing
    links or other inline elements with the text inside of the link)
    
    The right click context menu is no longer ~0.3em too long on the right
    side
    
    - ContextMenu objects now have an open and close method that will make
    the contextmenu appear or disappear respectively
    
    - Made updateOverlay() slightly faster by appending the child nodes to
    their parent BEFORE adding the parent to the DOM (prevents the browser
    from updating the DOM as much)
    
    - Adjacent text nodes are automatically combined for each call to
    updateOverlay
    
    - Removed a bug where adding an element to your current selection would
    quickly make a selection range (which colors the text of the page blue
    and is the default behavior of most browsers) and then delete it
    
    - Most keyboard shortcuts are now disabled if a dialog is open and
    blocking the document editor
    
    - ContextMenu.js is now unminified because it's such a pain to edit it
    in its minified form. It will eventually be replaced again with a
    minfied version for faster loading time, but not while it's still being
    worked on

--- script/ContextMenu.js
+++ script/ContextMenu.js
@@ -1,10 +1,229 @@
-(window.HTMLStudio=window.HTMLStudio||{}).ContextMenu=function ContextMenu(a){this.getItem=function(b){for(var a=this.node.children,c=a.length-1;0<=c;c--)if(a[c].getAttribute('data-identifier')==b||a[c].id==this.id+"-item-"+(b||0))return a[c]};this.node=document.createElement("ul");this.node.root=this.node;this.id=this.node.id="context-"+(new Date).getTime().toString(36);this.node.className="contextmenu";this.node.style.listStyle="none";this.node.style.display="inline-block";this.node.style.position="absolute";this.node.style.left=this.node.style.top=
-"0";this.node.style.zIndex=9999996;for(var c=a.items.length-1;0<=c;c--)(function(){var b=document.createElement("li"),d=document.createElement("div"),f=document.createElement("span"),g=document.createElement("div"),l="pad"in a.items[c]&&!a.items[c].pad,e=a.items[c];b.className="contextmenuitem";b.id=this.id+"-item-"+c;b.root=this.node;for(prop in a.items[c].attributes||{})b[prop]=a.items[c].attributes[prop];if(a.items[c].id)b.setAttribute('data-identifier',a.items[c].id);var h=a.items[c].disabled,m=a.items[c].toggled||!1;
-if(Object.defineProperties(b,{disabled:{get:function(){return h},set:function(a){h=!!a;(b.children[1]||b.firstChild).style.color=a?"#AAA":"";b.title=e[a&&e.disabledtitle?"disabledtitle":"title"];a&&e.disabledimage&&b.children[0].className.includes("contextmenuitemimage")?b.children[0].style.backgroundImage='url("'+e.disabledimage+'")':e.image&&b.children[0].className.includes("contextmenuitemimage")&&(b.children[0].style.backgroundImage='url("'+e.image+'")')},enumerable:!0,configurable:!0},toggled:{get:function(){return m},
-set:function(a){m=!!a;e.toggle&&!l&&(b.firstChild.style.backgroundImage='url("'+(a?e.image||"":e.imageoff||e.image||"")+'")')}}}),b.func=a.items[c].func,l?b.style.width="calc(12rem + 1.7em)":(a.items[c].toggle?d.style.backgroundImage=a.items[c].toggled?'url("'+a.items[c].image||'")':'url("'+(a.items[c].imageoff||a.items[c].image||"")+'")':!a.items[c].image||a.items[c].disabled&&a.items[c].disabledimage?a.items[c].disabledimage&&a.items[c].disabled&&(d.style.backgroundImage='url("'+a.items[c].disabledimage+'")'):d.style.backgroundImage=
-'url("'+a.items[c].image+'")',d.className="contextmenuitemimage",d.style.width="1.4em",d.style.height="1.4em",d.style.backgroundSize="100% 100%",d.style.display="inline-block",d.style.position="absolute",d.style.left=".3em",d.style.top="0em",d.style.pointerEvents="none",d.root=this.node,b.style.paddingLeft="2.1em",b.appendChild(d)),g.style.width="auto",g.style.textOverflow="ellipsis",g.style.overflowX="hidden",g.style.pointerEvents="none",g.root=this.node,g.className="contextmenuitemtextcontainer",h&&(g.style.color="#AAA"),b.appendChild(g),
-f.className="contextmenuitemtext",f.innerHTML=a.items[c].name||" ",f.style.pointerEvents="none",f.style.MozUserSelect="-moz-none",f.style.WebkitUserSelect="none",f.style.MsUserSelect="none",f.style.userSelect="none",f.root=this.node,g.appendChild(f),!a.items[c].title||h&&a.items[c].disabledtitle?a.items[c].disabledtitle&&h&&(b.title=a.items[c].disabledtitle):b.title=a.items[c].title,a.items[c].func&&b.addEventListener("click",function(a){h||b.func.call(this,a,function(){b.style.background="#F5F5F5";
-b.root.parentNode&&b.root.parentNode.removeChild(b.root)})}),b.addEventListener("mouseenter",function(){if(this.style.backgroundColor=h?"#E5E5E5":"#B2EBF2",this.style.whiteSpace="pre-line",this.style.height="",e.subcontext){var a=new HTMLStudio.ContextMenu(e.subcontext),c=b.getBoundingClientRect();a.node.parent=b;a.node.className+=" subcontextmenu";a.node.style.top=em(-.2)-1+"px";a.node.style.left=c.width+"px";a.node.style.position="absolute";this.appendChild(a.node)}}),b.addEventListener("mouseleave",
-function(a){if(this.style.backgroundColor="",this.style.whiteSpace="nowrap",this.style.height="1.4em",e.subcontext)for(a=this.children.length-1;0<=a;a--)this.children[a].className.includes("subcontextmenu")&&this.removeChild(this.children[a])}),b.addEventListener("mousedown",function(){this.style.backgroundColor=h?"#E5E5E5":"#A0D3D9"}),b.addEventListener("mouseup",function(){this.style.backgroundColor=h?"#E5E5E5":"#B2EBF2"}),b.style.padding=b.style.paddingLeft?".05em .4em .05em 2.1em":".05em .4em",
-b.style.paddingTop=".075em",b.style.paddingBottom=".025em",b.style.paddingRight=".4em",b.style.paddingLeft=b.style.paddingLeft||".4em",b.style.width=b.style.width||"12rem",b.style.height=b.style.minHeight="1.4em",b.style.textOverflow="ellipsis",b.style.whiteSpace="nowrap",b.style.position="relative",a.items[c].separate)b.style.marginBottom="calc(.2em + 1px)",d=document.createElement("div"),d.className="contextmenuitemseparator",d.style.width="calc(12rem + 1.7em)",d.style.height="1px",d.style.backgroundColor=
-"#C5C5C5",d.style.position="relative",d.style.top="-.15em",d.style.left=".4em",this.node.insertBefore(d,this.node.firstChild);if(a.css)for(var k in a.css)b.style[k]=a.css[k];if(a.items[c].css)for(k in a.items[c].css)b.style[k]=a.items[c].css[k];this.node.insertBefore(b,this.node.firstChild)}).call(this)};
+(window.HTMLStudio = window.HTMLStudio || {}).ContextMenu = function ContextMenu (args) {
+	function close() {
+		if (this.parentNode) this.parentNode.removeChild(this);
+		Array.prototype.forEach.call(this.children, function(child) {
+			if (child.nodeName == 'LI') {
+				child.style.backgroundColor = '';
+				child.style.whiteSpace = 'nowrap';
+				child.style.height = '1.4em';
+				if (child.arg.subcontext) {
+					for (var i = child.children.length - 1; i >= 0; i--) {
+						if (child.children[i].className.includes('subcontextmenu')) child.removeChild(child.children[i]);
+					}
+				}
+			}
+		});
+	}
+
+	function keyEvents(e) {
+		if (e.keyCode == 40) {
+			e.stopPropagation();
+			(this.nextElementSibling ? this.nextElementSibling.nodeName == 'LI' ? this.nextElementSibling : this.nextElementSibling.nextElementSibling : this.parentNode.firstElementChild).focus();
+		} else if (e.keyCode == 38) {
+			e.stopPropagation();
+			(this.previousElementSibling ? this.previousElementSibling.nodeName == 'LI' ? this.previousElementSibling : this.previousElementSibling.previousElementSibling : args.pseudoParent || this.parentNode.lastElementChild).focus();
+		} else if (e.keyCode == 39 && this.arg.subcontext) {
+			e.stopPropagation();
+			this.lastElementChild.firstElementChild.focus();
+		} else if (e.keyCode == 37 && this.root.parent && this.root.parent.className.includes('contextmenuitem')) {
+			e.stopPropagation();
+			this.root.parent.focus();
+		}
+	}
+
+	this.argument = args;
+	this.node = document.createElement('ul');
+	this.close = close.bind(this.node);
+	this.node.close = close.bind(this.node);
+	this.node.root = this.node;
+	this.id = this.node.id = 'context-' + new Date().getTime().toString(36);
+	this.node.className = 'contextmenu';
+	this.node.style.listStyle = 'none';
+	this.node.style.display = 'inline-block';
+	this.node.style.position = 'absolute';
+	this.node.style.left = this.node.style.top = 0;
+	this.node.style.zIndex = 9999996;
+	for (var i = args.items.length - 1; i >= 0; i--) (function(){
+		var li = document.createElement('li'),
+			image = document.createElement('div'),
+			text = document.createElement('span'),
+			textcontainer = document.createElement('div'),
+			nopad = 'pad' in args.items[i] && !args.items[i].pad,
+			itemplaceholder = li.arg = args.items[i],
+			disabled = args.items[i].disabled,
+			toggled = args.items[i].toggled || false;
+
+		li.className = 'contextmenuitem';
+		li.id = this.id + '-item-' + i;
+		li.tabIndex = 0;
+		li.root = this.node;
+
+		for (prop in args.items[i].attributes || {}) li[prop] = args.items[i].attributes[prop];
+        if (args.items[i].id) li.setAttribute('data-identifier', args.items[i].id);
+
+		Object.defineProperties(li, {
+			disabled: {
+				get: function() {return disabled},
+				set: function(v) {
+					disabled = !!v;
+					(li.children[1] || li.firstChild).style.color = v ? '#AAA' : '';
+					li.title = itemplaceholder[v && itemplaceholder.disabledtitle ? 'disabledtitle' : 'title'];
+					if (v && itemplaceholder.disabledimage && li.children[0].className.includes('contextmenuitemimage')) li.children[0].style.backgroundImage = 'url("' + itemplaceholder.disabledimage + '")';
+					else if (itemplaceholder.image && li.children[0].className.includes('contextmenuitemimage')) li.children[0].style.backgroundImage = 'url("' + itemplaceholder.image + '")';
+				},
+				enumerable: true,
+				configurable: true
+			},
+			toggled: {
+				get: function() {return toggled},
+				set: function(v) {
+					toggled = !!v;
+					if (itemplaceholder.toggle && !nopad) (li.firstChild).style.backgroundImage = 'url("' + (v ? itemplaceholder.image || '' : itemplaceholder.imageoff || itemplaceholder.image || '') + '")'
+				}
+			}
+		})
+		li.func = args.items[i].func;
+		li.addEventListener('keydown', keyEvents);
+
+		// Sets image if pad is true or not included
+		if (nopad) {
+			li.style.width = 'calc(12rem + 1.7em)';
+		} else {
+			if (args.items[i].toggle) image.style.backgroundImage = args.items[i].toggled ? 'url("' + args.items[i].image || '' + '")' : 'url("' + (args.items[i].imageoff || args.items[i].image || '') + '")';
+			else if (args.items[i].image && (!args.items[i].disabled || !args.items[i].disabledimage)) image.style.backgroundImage = 'url("' + args.items[i].image + '")';
+			else if (args.items[i].disabledimage && args.items[i].disabled) image.style.backgroundImage = 'url("' + args.items[i].disabledimage + '")';
+			// Sets image styles
+			image.className = 'contextmenuitemimage'
+			image.style.width = '1.4em';
+			image.style.height = '1.4em';
+			image.style.backgroundSize = '100% 100%';
+			image.style.display = 'inline-block';
+			image.style.position = 'absolute';
+			image.style.left = '.3em';
+			image.style.top = '0em';
+			image.style.pointerEvents = 'none';
+			image.root = this.node;
+			li.style.paddingLeft = '2.1em';
+			li.appendChild(image);
+		}
+		textcontainer.style.width = 'auto';
+		textcontainer.style.textOverflow = 'ellipsis';
+		textcontainer.style.overflowX = 'hidden';
+		textcontainer.style.pointerEvents = 'none';
+		textcontainer.root = this.node;
+		textcontainer.className = 'contextmenuitemtextcontainer';
+		if (disabled) textcontainer.style.color = '#AAA';
+		li.appendChild(textcontainer)
+		// Sets the item's text
+		text.className = 'contextmenuitemtext';
+		text.innerHTML = args.items[i].name || ' ';
+		text.style.pointerEvents = 'none';
+		text.style.MozUserSelect = '-moz-none';
+		text.style.WebkitUserSelect = 'none';
+		text.style.MsUserSelect = 'none';
+		text.style.userSelect = 'none';
+		text.root = this.node;
+		textcontainer.appendChild(text);
+		// Sets the item's title if one is defined
+		if (args.items[i].title && (!disabled || !args.items[i].disabledtitle)) li.title = args.items[i].title;
+		else if (args.items[i].disabledtitle && disabled) li.title = args.items[i].disabledtitle;
+		// Sets the item's callback function if one is defined
+		if (args.items[i].func) {
+			li.addEventListener('click', function(e) {
+				if (!disabled) li.func.call(this, e, function() {
+					li.style.background = '#F5F5F5';
+					close.call(li.root);
+				});
+			});
+			li.addEventListener('keydown', function(e) {
+				if (e.keyCode == 13 && !disabled) li.func.call(this, e, function() {
+					li.style.background = '#F5F5F5';
+					close.call(li.root);
+				})
+			})
+		};
+		// Sets the item's hover and click color behavior
+		['mouseenter','focus'].forEach(function(event) {
+			li.addEventListener(event, function() {
+				this.style.backgroundColor = disabled ? '#E5E5E5' : '#B2EBF2';
+				this.style.whiteSpace = 'pre-line';
+				this.style.height = '';
+				if (itemplaceholder.subcontext) {
+					var context = new ContextMenu(itemplaceholder.subcontext), rect = li.getBoundingClientRect();
+					context.node.parent = li;
+					context.node.className += ' subcontextmenu';
+					context.node.style.top = em(-.2) - 1 + 'px';
+					context.node.style.left = rect.width + 'px';
+					context.node.style.position = 'absolute';
+					this.appendChild(context.node)
+				}
+			});
+		});
+		
+		['mouseleave','blur'].forEach(function(event) {
+			li.addEventListener(event, function(e){
+				this.style.backgroundColor = '';
+				this.style.whiteSpace = 'nowrap';
+				this.style.height = '1.4em';
+				if (itemplaceholder.subcontext) {
+					function f() {
+						for (var i = li.children.length - 1; i >= 0; i--) {
+							if (li.children[i].className.includes('subcontextmenu')) li.removeChild(li.children[i]);
+						}
+					}
+
+					if (e.type == 'blur') setTimeout(function() {
+						if (!(document.activeElement.root && document.activeElement.root.parent == li)) f();
+					},0);
+					else f();
+				}
+			});
+		});
+
+		li.addEventListener('mousedown', function(){this.style.backgroundColor = disabled ? '#E5E5E5' : '#A0D3D9'});
+		li.addEventListener('mouseup', function(){this.style.backgroundColor = disabled ? '#E5E5E5' : '#B2EBF2'});
+		li.style.padding = li.style.paddingLeft ? '.05em .4em .05em 2.1em' : '.05em .4em';
+		li.style.paddingTop = '.075em';
+		li.style.paddingBottom = '.025em';
+		li.style.paddingRight = '.4em';
+		li.style.paddingLeft = li.style.paddingLeft || '.4em';
+		li.style.width = li.style.width || '12rem';
+		li.style.height = li.style.minHeight = '1.4em';
+		li.style.textOverflow = 'ellipsis';
+		li.style.whiteSpace = 'nowrap';
+		li.style.position = 'relative';
+		if (args.items[i].separate) {
+			li.style.marginBottom = 'calc(.2em + 1px)';
+			var separator = document.createElement('div');
+			separator.className = 'contextmenuitemseparator';
+			separator.style.width = 'calc(12rem + 1.7em)';
+			separator.style.height = '1px';
+			separator.style.backgroundColor = '#C5C5C5';
+			separator.style.position = 'relative';
+			separator.style.top = '-.15em';
+			separator.style.left = '.4em';
+			this.node.insertBefore(separator, this.node.firstChild)
+		}
+		if (args.css) {
+			for (var item in args.css) {
+				li.style[item] = args.css[item];
+			}
+		}
+		if (args.items[i].css) {
+			for (var item in args.items[i].css) {
+				li.style[item] = args.items[i].css[item];
+			}
+		}
+		// Appends item to overall context menu
+		this.node.insertBefore(li, this.node.firstChild);
+	}).call(this)
+}
+
+HTMLStudio.ContextMenu.prototype.open = function open() {
+	document.body.insertBefore(this.node, document.getElementById('toolbarcontainer').nextElementSibling);
+}
+
+HTMLStudio.ContextMenu.prototype.getItem = function(arg) {
+	for (var children = this.node.children, i = children.length - 1; i >= 0; i--)
+		if (children[i].getAttribute('data-identifier') == arg || children[i].id == this.id + '-item-' + (arg || 0)) return children[i]
+};
